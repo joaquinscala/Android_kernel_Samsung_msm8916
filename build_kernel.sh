29a72c42ca7311b5017f73a4350749d3c0475e72
joaquinscala <joaquinscala@hotmail.com>
2016-05-29 16:32:14 -0300

    build_kernel: updates

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=/home/joaquinscala/arm-eabi-4.8/bin/arm-eabi-
 mkdir output
 
+make clean && make mrproper
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_a5_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+ 
+rm -rf joaquin_modules
+mkdir -p joaquin_modules
+make modules_install INSTALL_MOD_PATH=joaquin_modules INSTALL_MOD_STRIP=1
+
+
